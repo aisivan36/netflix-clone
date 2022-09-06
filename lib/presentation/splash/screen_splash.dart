89de646bbda78300ecad50aef64f832e09fc7b96
aisivan36 <aisivan36@gmail.com>
2022-09-06 10:45:52 +0700

    fixed data type object and others

--- lib/presentation/splash/screen_splash.dart
+++ lib/presentation/splash/screen_splash.dart
@@ -7,12 +7,13 @@ class ScreenSplash extends StatefulWidget {
   const ScreenSplash({Key? key}) : super(key: key);
 
   @override
-  _ScreenSplashState createState() => _ScreenSplashState();
+  State<ScreenSplash> createState() => _ScreenSplashState();
 }
 
 class _ScreenSplashState extends State<ScreenSplash> {
   @override
   void initState() {
+    super.initState();
     navigatorHome();
   }
 
@@ -20,6 +21,7 @@ class _ScreenSplashState extends State<ScreenSplash> {
     await Future.delayed(
       const Duration(seconds: 5),
     );
+    if (!mounted) return;
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => const ScreenOnStart(),
